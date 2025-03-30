887e76881673df85f679c400db4700962ee8f4ec
Francisco Juan <juansoussev@gmail.com>
2025-03-30 20:35:29 -0300

    first commit

---
+++ class01/main.go
@@ -0,0 +1,12 @@
+// Initializing main.go
+
+// All of files must have package to define as a module, similar to Java
+package main
+
+// Imports are make like that
+import "fmt"
+
+// main funcion is the main(KKKKKKKKKKK)
+func main() {
+	fmt.Println("Hello World")
+}
